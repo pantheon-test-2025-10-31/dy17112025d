99dba8a1cfadee4943dff32b869c2e82f4cef021
Danish <danishyasin33@gmail.com>
2026-02-02 17:09:58 -0800

    remove cacheHandler dir

--- cacheHandler/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all cache handlers
-export { default as FileCacheHandler, getSharedCacheStats, clearSharedCache } from './file-cache-handler';
-export { default as MemoryCacheHandler, cache } from './memory-cache-handler';
-
-// Export types
-export * from './types';
